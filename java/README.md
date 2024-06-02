1514f35187a4cb165db1ca2e6b649e74ac9aa8a0
Radik <safinrraf@ymail.com>
2024-06-02 20:38:40 +0200

    code for ch1

---
+++ java/README.md
@@ -0,0 +1,3 @@
+# About examples
+
+This folder contains Scala and Python versions of the Java code used in Manning Publication’s **[Spark in Action, 2nd edition](https://www.manning.com/books/spark-in-action-second-edition?a_aid=jgp)**, by Jean-Georges Perrin.
---
+++ java/ch1_gradle/app/src/main/java/org/example/CsvToDataframeApp.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * CSV ingestion in a dataframe.
+ * 
+ * @author jgp
+ */
+public final class CsvToDataframeApp {
+
+  /**
+   * main() is your entry point to the application.
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    final var app = new CsvToDataframeApp();
+    app.start();
+  }
+
+  /**
+   * The processing code.
+   */
+  private void start() {
+    // Creates a session on a local master
+    final var spark = SparkSession.builder()
+        .appName("CSV to Dataset")
+        .master("local")
+        .getOrCreate();
+
+    // Reads a CSV file with header, called books.csv, stores it in a
+    // dataframe
+    Dataset<Row> df = spark.read().format("csv")
+        .option("header", "true")
+        .load("data/books.csv");
+
+    // Shows at most 5 rows from the dataframe
+    df.show(5);
+  }
+
+  public String getGreeting() {
+    return "Hello World!";
+}
+}
